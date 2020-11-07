04552e4288d1a67ba546fa9aa07572645d9d4925
fdabrowski <filip.dabrowski.pg@gmail.com>
2020-11-07 14:29:21 +0100

    add repository annotation to guests and reservations

--- src/main/java/com/filipdabrowski/lil/springbootdemo/data/repository/GuestRepository.java
+++ src/main/java/com/filipdabrowski/lil/springbootdemo/data/repository/GuestRepository.java
@@ -3,7 +3,9 @@ package com.filipdabrowski.lil.springbootdemo.data.repository;
 import com.filipdabrowski.lil.springbootdemo.data.entity.Guest;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GuestRepository extends CrudRepository <Guest, Long> {
-    
+
 }
--- src/main/java/com/filipdabrowski/lil/springbootdemo/data/repository/ReservationRepository.java
+++ src/main/java/com/filipdabrowski/lil/springbootdemo/data/repository/ReservationRepository.java
@@ -3,7 +3,9 @@ package com.filipdabrowski.lil.springbootdemo.data.repository;
 import com.filipdabrowski.lil.springbootdemo.data.entity.Reservation;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
     
 }
